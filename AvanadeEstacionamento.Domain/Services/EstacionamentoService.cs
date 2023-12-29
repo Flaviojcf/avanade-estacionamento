1d9dfbb44c733350a92e2e14297dd92f0606494f
Flavio <fjcf@poli.br>
2023-12-29 12:26:52 -0300

    feat: Implements veiculo and estacionamento service

---
+++ AvanadeEstacionamento.Domain/Services/EstacionamentoService.cs
@@ -0,0 +1,66 @@
+﻿using AvanadeEstacionamento.Domain.Interfaces.Repository;
+using AvanadeEstacionamento.Domain.Interfaces.Service;
+using AvanadeEstacionamento.Domain.Models;
+
+namespace AvanadeEstacionamento.Domain.Services
+{
+    public class EstacionamentoService : IEstacionamentoService
+    {
+
+        #region Dependency Injection
+
+        private readonly IEstacionamentoRepository _estacionamentoRepository;
+
+        #endregion
+
+        #region Constructor
+
+        public EstacionamentoService(IEstacionamentoRepository estacionamentoRepository)
+        {
+            _estacionamentoRepository = estacionamentoRepository;
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public async Task<IEnumerable<EstacionamentoModel>> GetAll()
+        {
+            return await _estacionamentoRepository.GetAll();
+        }
+
+        public async Task<EstacionamentoModel> GetById(Guid id)
+        {
+            return await _estacionamentoRepository.GetById(id);
+        }
+
+        public async Task<EstacionamentoModel> Create(EstacionamentoModel estacionamento)
+        {
+            await _estacionamentoRepository.Create(estacionamento);
+            return estacionamento;
+        }
+
+        public async Task<bool> Delete(Guid id)
+        {
+            return await _estacionamentoRepository.Delete(id);
+        }
+
+        public async Task<bool> Update(EstacionamentoModel estacionamento, Guid id)
+        {
+            if (estacionamento.Id != id) return false;
+
+            var result = await _estacionamentoRepository.Update(estacionamento);
+
+            if (result)
+            {
+                return true;
+            }
+            else
+            {
+                throw new Exception("Falha ao editar o estacionamento");
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ AvanadeEstacionamento.Domain/Services/VeiculoService.cs
@@ -0,0 +1,82 @@
+﻿using AvanadeEstacionamento.Domain.Interfaces.Repository;
+using AvanadeEstacionamento.Domain.Interfaces.Service;
+using AvanadeEstacionamento.Domain.Models;
+
+namespace AvanadeEstacionamento.Domain.Services
+{
+    public class VeiculoService : IVeiculoService
+    {
+
+        #region Dependency Injection
+
+        private readonly IVeiculoRepository _veiculoRepository;
+
+        #endregion
+
+        #region Constructor
+
+        public VeiculoService(IVeiculoRepository veiculoRepository)
+        {
+            _veiculoRepository = veiculoRepository;
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public async Task<IEnumerable<VeiculoModel>> GetAll()
+        {
+            return await _veiculoRepository.GetAll();
+        }
+
+        public async Task<IEnumerable<VeiculoModel>> GetByEstacionamentoId(Guid id)
+        {
+            return await _veiculoRepository.GetByEstacionamentoId(id);
+        }
+
+        public async Task<VeiculoModel> GetById(Guid id)
+        {
+            return await _veiculoRepository.GetById(id);
+        }
+
+        public async Task<VeiculoModel> Create(VeiculoModel veiculo)
+        {
+            await _veiculoRepository.Create(veiculo);
+
+            return veiculo;
+        }
+
+        public async Task<bool> Delete(Guid id)
+        {
+            var result = await _veiculoRepository.Delete(id);
+
+            if (result)
+            {
+                return true;
+            }
+            else
+            {
+                throw new Exception("Falha ao deletar veículo");
+            }
+        }
+
+        public async Task<bool> Update(VeiculoModel veiculo, Guid id)
+        {
+            if (veiculo.Id != id) return false;
+
+            var result = await _veiculoRepository.Update(veiculo);
+
+            if (result)
+            {
+                return true;
+            }
+            else
+            {
+                throw new Exception("Falha ao editar o veiculo");
+            }
+        }
+
+        #endregion
+
+    }
+}
