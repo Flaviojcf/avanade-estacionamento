24b27b83fff12355264e6eb568603023a07ec504
Flavio <fjcf@poli.br>
2023-12-29 12:38:57 -0300

    feat: Implements Estacionamento and veiculo repository

---
+++ AvanadeEstacionamento.Data/Repository/EstacionamentoRepository.cs
@@ -0,0 +1,17 @@
+﻿using AvanadeEstacionamento.Data.Context;
+using AvanadeEstacionamento.Domain.Interfaces.Repository;
+using AvanadeEstacionamento.Domain.Models;
+
+namespace AvanadeEstacionamento.Data.Repository
+{
+    public class EstacionamentoRepository : BaseRepository<EstacionamentoModel>, IEstacionamentoRepository
+    {
+
+        #region Constructor
+
+        public EstacionamentoRepository(MyDbContext context) : base(context) { }
+
+        #endregion
+
+    }
+}
