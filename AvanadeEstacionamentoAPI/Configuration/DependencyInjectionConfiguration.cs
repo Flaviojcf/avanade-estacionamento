6b5832c3fdddaf39697adb4d2fdfeb92e1371162
Flavio <fjcf@poli.br>
2023-12-29 12:46:51 -0300

    feat: Create DependencyInjectionConfiguration

---
+++ AvanadeEstacionamentoAPI/Configuration/DependencyInjectionConfiguration.cs
@@ -0,0 +1,13 @@
+﻿namespace AvanadeEstacionamento.API.Configuration
+{
+    public static class DependencyInjectionConfiguration
+    {
+        public static IServiceCollection ResolveDependencies(this IServiceCollection services)
+        {
+            services.AddScoped<MyDbContext>();
+
+
+            return services;
+        }
+    }
+}
