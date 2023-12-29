68a30a3dd0098c84b79259a8df25e7260beaa857
Flavio <fjcf@poli.br>
2023-12-29 19:07:42 -0300

    feat(database): add new columns

---
+++ AvanadeEstacionamento.Data/Migrations/20231229220602_createNewColumns.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AvanadeEstacionamento.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class createNewColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DataCriacao",
+                table: "Veiculo",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAtivo",
+                table: "Veiculo",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DataCriacao",
+                table: "Estacionamento",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAtivo",
+                table: "Estacionamento",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DataCriacao",
+                table: "Veiculo");
+
+            migrationBuilder.DropColumn(
+                name: "IsAtivo",
+                table: "Veiculo");
+
+            migrationBuilder.DropColumn(
+                name: "DataCriacao",
+                table: "Estacionamento");
+
+            migrationBuilder.DropColumn(
+                name: "IsAtivo",
+                table: "Estacionamento");
+        }
+    }
+}
