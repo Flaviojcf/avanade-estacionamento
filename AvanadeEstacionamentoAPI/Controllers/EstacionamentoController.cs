a12f033846063935e93d03b59c4c3eecec8cfa57
Flavio <fjcf@poli.br>
2023-12-29 13:05:23 -0300

    feat: Create estacionamento and veiculo controller

---
+++ AvanadeEstacionamentoAPI/Controllers/EstacionamentoController.cs
@@ -0,0 +1,69 @@
+﻿using AvanadeEstacionamento.Domain.Interfaces.Service;
+using AvanadeEstacionamento.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AvanadeEstacionamento.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EstacionamentoController : ControllerBase
+    {
+
+        #region Dependency Injection
+
+        private readonly IEstacionamentoService _estacionamentoService;
+
+        #endregion
+
+        #region Constructor
+
+        public EstacionamentoController(IEstacionamentoService estacionamentoService)
+        {
+            _estacionamentoService = estacionamentoService;
+        }
+
+        #endregion
+
+        #region HTTP Requests
+
+        [HttpGet]
+        public async Task<ActionResult> GetAll()
+        {
+            var result = await _estacionamentoService.GetAll();
+            return Ok(result);
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<ActionResult> GetById(Guid id)
+        {
+            var result = await _estacionamentoService.GetById(id);
+            return Ok(result);
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult> Create(EstacionamentoModel estacionamentoModel)
+        {
+            var result = await _estacionamentoService.Create(estacionamentoModel);
+            return Ok(result);
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<ActionResult> Delete(Guid id)
+        {
+            var result = await _estacionamentoService.Delete(id);
+
+            return Ok(result);
+        }
+
+        [HttpPut("{id:guid}")]
+        public async Task<ActionResult> Update(EstacionamentoModel estacionamentoModel, Guid id)
+        {
+            var result = await _estacionamentoService.Update(estacionamentoModel, id);
+
+            return Ok(result);
+        }
+
+        #endregion
+    }
+}
---
+++ AvanadeEstacionamentoAPI/Controllers/VeiculoController.cs
@@ -0,0 +1,77 @@
+﻿using AvanadeEstacionamento.Domain.Interfaces.Service;
+using AvanadeEstacionamento.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AvanadeEstacionamento.API.Controllers
+{
+    [Route("api/veiculo")]
+    [ApiController]
+    public class VeiculoController : ControllerBase
+    {
+
+        #region Dependency Injection
+
+        private readonly IVeiculoService _veiculoService;
+
+        #endregion
+
+        #region Constructor
+
+        public VeiculoController(IVeiculoService veiculoService)
+        {
+            _veiculoService = veiculoService;
+        }
+
+        #endregion
+
+        #region HTTP Requests
+
+        [HttpGet]
+        public async Task<ActionResult> GetAll() 
+        {
+            var result = await _veiculoService.GetAll();
+            return Ok(result);
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<ActionResult> GetById(Guid id) 
+        {
+            var result = await _veiculoService.GetById(id);
+            return Ok(result);
+        }
+
+
+        [HttpGet("{id:guid}")]
+        public async Task<ActionResult> GetByEstacionamentoId(Guid id)
+        {
+            var result = await _veiculoService.GetByEstacionamentoId(id);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(VeiculoModel veiculoModel)
+        {
+            var result = await _veiculoService.Create(veiculoModel);
+            return Ok(result);
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<ActionResult> Delete(Guid id)
+        {
+            var result = await _veiculoService.Delete(id);
+
+            return Ok(result);
+        }
+
+        [HttpPut("{id:guid}")]
+        public async Task<ActionResult> Update(VeiculoModel veiculoModel, Guid id)
+        {
+            var result = await _veiculoService.Update(veiculoModel, id);
+
+            return Ok(result);
+        }
+
+        #endregion
+
+    }
+}
