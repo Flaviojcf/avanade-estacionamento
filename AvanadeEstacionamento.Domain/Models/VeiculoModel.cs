76a489fecbe78c916bac0c9edfb160caf0ab1ce7
Flavio <fjcf@poli.br>
2023-12-28 23:57:39 -0300

    feat: Add EstacionamentoModel reference to relation 1-n

--- AvanadeEstacionamento.Domain/Models/VeiculoModel.cs
+++ AvanadeEstacionamento.Domain/Models/VeiculoModel.cs
@@ -4,5 +4,10 @@ namespace AvanadeEstacionamento.Domain.Models
     public class VeiculoModel : BaseModel
     {
         public string Placa { get; set; }
+
+
+        public int EstacionamentoId { get; set; }
+        public EstacionamentoModel Estacionamento { get; set; }
+
     }
 }
