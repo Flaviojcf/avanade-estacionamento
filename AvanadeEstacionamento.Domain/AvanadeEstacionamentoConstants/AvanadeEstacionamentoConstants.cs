353b39c7bfdaaa9a935fc9b143ebd3f2a40d9525
Flavio <fjcf@poli.br>
2024-01-02 21:39:37 -0300

    feat: Create all constants for validation exception in estacionamento and veiculo service

--- AvanadeEstacionamento.Domain/AvanadeEstacionamentoConstants/AvanadeEstacionamentoConstants.cs
+++ AvanadeEstacionamento.Domain/AvanadeEstacionamentoConstants/AvanadeEstacionamentoConstants.cs
@@ -4,18 +4,34 @@
     {
         #region Constants for veiculo exceptions
 
-        public const string VEICULO_NOT_FOUND_EXCEPTION = "Nenhum veículo cadastrado.";
+        public const string VEICULO_NOT_FOUND_EXCEPTION = "Veículo não encontrado.";
 
-        public const string VEICULO_BY_ESTACIONAMENTO_NOT_FOUND = "Nenhum veículo vinculado a este estacionamento foi encontrado.";
+        public const string ANY_VEICULO_HAS_BEEN_REGISTERED_EXCEPTION = "Nenhum veículo cadastrado.";
 
-        public const string VEICULO_BY_PLACA_ALREADY_EXISTS = "A placa informada já foi cadastrada, realize uma busca para verificar as informações completas do veículo.";
+        public const string VEICULO_BY_ESTACIONAMENTO_NOT_FOUND_EXCEPTION = "Nenhum veículo vinculado a este estacionamento foi encontrado.";
 
-        public const string VEICULO_DELETE_FAIL = "Falha ao deletar veículo.";
+        public const string VEICULO_BY_PLACA_ALREADY_EXISTS_EXCEPTION = "A placa informada já foi cadastrada, realize uma busca para verificar as informações completas do veículo.";
 
-        public const string VEICULO_UPDATE_FAIL = "Falha ao atualizar o veículo. O ID fornecido não corresponde ao ID do veículo repassado.";
+        public const string VEICULO_DELETE_FAIL_EXCEPTION = "Falha ao deletar veículo.";
+
+        public const string VEICULO_UPDATE_FAIL_EXCEPTION = "Falha ao atualizar o veículo. O ID fornecido não corresponde ao ID do veículo repassado.";
 
         #endregion
 
+
+        #region Constants for estacionamento exceptions
+
+        public const string ESTACIONAMENTO_NOT_FOUND_EXCEPTION = "Veículo não encontrado.";
+
+        public const string ANY_ESTACIONAMENTO_HAS_BEEN_REGISTERED_EXCEPTION = "Nenhum estacionamento cadastrado.";
+
+        public const string ESTACIONAMENTO_DELETE_FAIL_EXCEPTION = "Falha ao deletar estacionamento.";
+
+        public const string ESTACIONAMENTO_UPDATE_FAIL_EXCEPTION = "Falha ao atualizar o estacionamento. O ID fornecido não corresponde ao ID do estacionamento repassado.";
+
+        #endregion
+
+        public const string GLOBAL_MESSAGE_FOR_INTERNAL_ERROR_EXCEPTION = "Ocorreu um erro interno. Entre em contato com nossa equipe para mais informações. Visite: https://github.com/Flaviojcf ";
 
     }
 }
