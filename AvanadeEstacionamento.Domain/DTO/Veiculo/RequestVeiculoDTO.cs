887bb3da4fdfd617061eaafef848098762097e60
Flavio <fjcf@poli.br>
2024-01-03 16:29:28 -0300

    refactor: Add validations for DTO's

--- AvanadeEstacionamento.Domain/DTO/Veiculo/RequestVeiculoDTO.cs
+++ AvanadeEstacionamento.Domain/DTO/Veiculo/RequestVeiculoDTO.cs
@@ -1,11 +1,15 @@
-﻿namespace AvanadeEstacionamento.Domain.DTO.Veiculo
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AvanadeEstacionamento.Domain.DTO.Veiculo
 {
     public class RequestVeiculoDTO
     {
-        public Guid? Id { get; set; }
 
+        [Required(ErrorMessage = "É preciso informar a placa do veículo.")]
+        [RegularExpression(@"^[A-Za-z]{3}-\d{4}$", ErrorMessage = "Placa inválida, formato deve ser ABC-1234.")]
         public string Placa { get; set; }
 
+        [Required(ErrorMessage = "É preciso informar um id de estacionamento.")]
         public Guid EstacionamentoId { get; set; }
 
     }
